525578f70437246d923822dbeafd71da715434e6
Kostas Demiris <kostasdemiris@gmail.com>
2026-01-22 15:30:38 +0200

    Fix TypeScript contract types compilation and exports

--- ts/types/contracts/index.ts
+++ ts/types/contracts/index.ts
@@ -18,11 +18,40 @@ export type {
   QueryMsg as TreasuryQueryMsg,
 } from "./treasury/ts/Treasury.types";
 
-// Account contract types - with prefixed names to avoid conflicts
+// Treasury contract clients (cosmjs-based)
+export {
+  TreasuryQueryClient,
+  TreasuryClient,
+  type TreasuryReadOnlyInterface,
+  type TreasuryInterface,
+} from "./treasury/ts/Treasury.client";
+
+// Treasury message composer (cosmjs-based)
+export {
+  TreasuryMsgComposer,
+  type TreasuryMsg,
+} from "./treasury/ts/Treasury.message-composer";
+
+// Account contract types
 export type {
   Binary as AccountBinary,
   InstantiateMsg as AccountInstantiateMsg,
   ExecuteMsg as AccountExecuteMsg,
   QueryMsg as AccountQueryMsg,
   AddAuthenticator,
+  MigrateMsg as AccountMigrateMsg,
 } from "./account/ts/Account.types";
+
+// Account contract clients (cosmjs-based)
+export {
+  AccountQueryClient,
+  AccountClient,
+  type AccountReadOnlyInterface,
+  type AccountInterface,
+} from "./account/ts/Account.client";
+
+// Account message composer (cosmjs-based)
+export {
+  AccountMsgComposer,
+  type AccountMsg,
+} from "./account/ts/Account.message-composer";
